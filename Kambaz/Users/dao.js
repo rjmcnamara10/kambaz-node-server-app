b58a384241f9f173aac74f1d746b30ae22c5ca58
rjmcnamara10 <rjmcnamara10@gmail.com>
2025-11-19 18:08:58 -0500

    working on 6.2.6.6 editing user email

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,25 +1,31 @@
-import { v4 as uuidv4 } from "uuid";
+import model from "./model.js";
 
-export default function UsersDao(db) {
-  let { users } = db;
+export default function UsersDao() {
+  const createUser = (user) => {};
 
-  const createUser = (user) => {
-    const newUser = { ...user, _id: uuidv4() };
-    users = [...users, newUser];
-    return newUser;
-  };
-  const findAllUsers = () => users;
-  const findUserById = (userId) => users.find((user) => user._id === userId);
+  const findAllUsers = () => model.find();
+
+  const findUserById = (userId) => model.findById(userId);
+
   const findUserByUsername = (username) =>
-    users.find((user) => user.username === username);
+    model.findOne({ username: username });
+
   const findUserByCredentials = (username, password) =>
-    users.find(
-      (user) => user.username === username && user.password === password
-    );
+    model.findOne({ username, password });
+
   const updateUser = (userId, user) =>
-    (users = users.map((u) => (u._id === userId ? user : u)));
-  const deleteUser = (userId) =>
-    (users = users.filter((u) => u._id !== userId));
+    model.updateOne({ _id: userId }, { $set: user });
+
+  const deleteUser = (userId) => model.deleteOne({ _id: userId });
+
+  const findUsersByRole = (role) => model.find({ role: role });
+
+  const findUsersByPartialName = (partialName) => {
+    const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+    return model.find({
+      $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+    });
+  };
 
   return {
     createUser,
@@ -29,5 +35,7 @@ export default function UsersDao(db) {
     findUserByCredentials,
     updateUser,
     deleteUser,
+    findUsersByRole,
+    findUsersByPartialName,
   };
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import session from "express-session";
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 import Lab5 from "./Lab5/index.js";
 import Hello from "./Hello.js";
@@ -11,6 +12,8 @@ import ModulesRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
 
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
